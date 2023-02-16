9b66ef5a4ed685414ea61901de3b84bbbfdb8e01
abdelrahman839 <asdddghjh@gmail.com>
2023-02-16 19:10:00 +0200

    fix issue

--- src/components/admin/SidebarFilter.js
+++ src/components/admin/SidebarFilter.js
@@ -2,24 +2,29 @@ import React from 'react'
 import styled from 'styled-components';
 import FilterContnet from './FilterContnet';
 import FilterAltIcon from '@mui/icons-material/FilterAlt';
-const SidebarFilter = ({IsService, IsCountry,IsCompany}) => {
+const SidebarFilter = ({ IsService, IsCountry, IsCompany }) => {
     return (
         <>
             <Container1>
-                <FilterContnet IsService={IsService} IsCompany={IsCompany} IsCountry={IsCountry}  />
+                <FilterContnet IsService={IsService} IsCompany={IsCompany} IsCountry={IsCountry} />
             </Container1>
             <Container2>
-                <button className="btn btn-secondary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBothOptions" aria-controls="offcanvasWithBothOptions"><FilterAltIcon/></button>
-                <div className="offcanvas offcanvas-start" data-bs-scroll="true"  id="offcanvasWithBothOptions" aria-labelledby="offcanvasWithBothOptionsLabel">
+
+                <button className="btn btn-secondary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
+                   <FilterAltIcon/>
+                </button>
+
+                <div className="offcanvas offcanvas-start" tabIndex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
                     <div className="offcanvas-header">
-                        <h5 className="offcanvas-title" id="offcanvasWithBothOptionsLabel">Filter</h5>
+                        <h5 className="offcanvas-title" id="offcanvasExampleLabel">Filter</h5>
                         <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                     </div>
                     <div className="offcanvas-body">
-                        <button className="btn btn-outline-secondary">btn</button>
-                        <FilterContnet IsService={IsService} IsCompany={IsCompany} IsCountry={IsCountry}/>
+                    <FilterContnet IsService={IsService} IsCompany={IsCompany} IsCountry={IsCountry} />
                     </div>
                 </div>
+
+              
 
             </Container2>
         </>
